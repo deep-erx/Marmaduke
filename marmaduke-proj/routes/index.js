381ca91ee3444673ba58e9515e35f21060e68d97
deep_erx <albertodeprezzo@gmail.com>
2018-09-26 18:16:48 +0200

    jslint refactoring

--- marmaduke-proj/routes/index.js
+++ marmaduke-proj/routes/index.js
@@ -1,26 +1,26 @@
 "use strict";
 
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let configuration = require('./data/configuration');
-let queries = require('./data/queries');
-let moment = require('moment')
+let configuration = require("./data/configuration");
+let queries = require("./data/queries");
+let moment = require("moment");
 
-const {Pool, client} = require('pg');
+const {Pool, client} = require("pg");
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Main page' });
+router.get("/", function(req, res) {
+  res.render("index", { title: "Main page" });
 });
 
-router.get('/api/moored_now/:id', (req, res, next) => {
-  
+router.get("/api/moored_now/:id", (req, res, next) => {
+
   const pool = new Pool(configuration.AUTH_DB);
   const id = req.params.id;
   const mdate = moment().format("YYYY-MM-DD");
 
   // async/await call
-  (async () => {
+  (async ()=> {
     const client = await pool.connect();
     try {
       const params = {
@@ -28,10 +28,10 @@ router.get('/api/moored_now/:id', (req, res, next) => {
         statesOfMooring: configuration.STATES_OF_MOORING,
         id: id
       };
-      
+
       let queryMoored = queries.mooredNow(params);
       const records = await client.query(queryMoored);
-      res.render('moored_now', {allTripsActive: records.rows});
+      res.render("moored_now", {allTripsActive: records.rows});
 
     } finally {
       client.release();
