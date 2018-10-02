cdbae0430ed4b72e86b157a9eea8e822b384b87c
deep_erx <albertodeprezzo@gmail.com>
2018-10-02 13:54:36 +0200

    test query - showing all moored ships

--- marmaduke-proj/routes/data/queries.js
+++ marmaduke-proj/routes/data/queries.js
@@ -1,5 +1,6 @@
 let fMooredNow = function(params){
-    const query = `SELECT id_control_unit_data, ship_description, 
+    const query = `SELECT 
+    id_control_unit_data, ship_description, 
     state_name, ts_main_event_field_val 
     FROM ships 
     INNER JOIN control_unit_data 
@@ -9,13 +10,9 @@ let fMooredNow = function(params){
     INNER JOIN states
     ON fk_state = id_state
     WHERE (is_active = true 
-    OR on_hold = true 
-    OR ts_archived >= '${params.mdate}')
-    AND fk_state in ${params.statesOfMooring}
-    AND LENGTH(ts_main_event_field_val) > 4 
-    AND DATE(ts_main_event_field_val) = '${params.mdate}'
+    OR on_hold = true) 
     AND control_unit_data.fk_portinformer = ${params.id}
-    ORDER BY ts_main_event_field_val`;
+    ORDER BY id_control_unit_data, ship_description, ts_main_event_field_val`;
 
     return query;
 }
