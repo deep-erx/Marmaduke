959e22da1c5069b52e32dd0696ef59f036cfdaa0
deeperx <albertodeprezzo@gmail.com>
2018-10-03 20:31:31 +0200

    decoupling db from general settings

---
+++ marmaduke-proj/routes/data/db_settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    AUTH_DB: {
+        user: 'test_user',
+        password: 'test_passwd',
+        database: 'test_db',
+        host: '127.0.0.1',
+        port: 5432
+    }
+}
