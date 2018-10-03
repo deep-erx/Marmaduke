c704d2cde93c203c1ad87e7972ee0f2f7040f3f8
Alberto de Prezzo <albertodeprezzo@gmail.com>
2018-10-04 00:31:02 +0200

    Delete db_settings.js

--- marmaduke-proj/routes/data/db_settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    AUTH_DB: {
-        user: 'test_user',
-        password: 'test_passwd',
-        database: 'test_db',
-        host: '127.0.0.1',
-        port: 5432
-    }
-}
