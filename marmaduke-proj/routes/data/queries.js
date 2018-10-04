42465ce83cf03551bed5f60da9d6e5ded84cb4e3
deeperx <albertodeprezzo@gmail.com>
2018-10-04 19:40:17 +0200

    static query optimization

--- marmaduke-proj/routes/data/queries.js
+++ marmaduke-proj/routes/data/queries.js
@@ -32,28 +32,18 @@ let fShipsNow = function(params){
 
 let fShipsStaticData = function(params){
     const query = `SELECT 
-    id_control_unit_data, ship_description,
-    MAX_TRIP.last_record AS last_record, 
-    array_states[cursor_now+1] AS act_state, state_name,
-    main_event_field
-    FROM states, ships
+    id_control_unit_data, ship_description, 
+    ts_main_event_field_val
+    FROM ships
     INNER JOIN control_unit_data
     ON fk_ship = id_ship
     INNER JOIN sequences
     ON control_unit_data.fk_portinformer = sequences.fk_portinformer
-    INNER JOIN (
-        SELECT fk_control_unit_data,
-        MAX(ts_main_event_field_val) AS last_record
-        FROM trips_logs
-        WHERE fk_state in ${params.statesOfInterest}
-        AND LENGTH(ts_main_event_field_val) > 4
-        AND DATE(ts_main_event_field_val) = '${params.mdate}' 
-        GROUP BY fk_control_unit_data
-    ) as MAX_TRIP 
-    ON MAX_TRIP.fk_control_unit_data = id_control_unit_data
-    WHERE array_states[cursor_now+1] = states.id_state
-    AND array_states[cursor_now+1] in ${params.statesOfInterest}
-    AND control_unit_data.fk_portinformer = ${params.id}
+    INNER JOIN trips_logs
+    ON fk_control_unit_data = id_control_unit_data 
+    WHERE LENGTH(ts_main_event_field_val) > 4
+    AND DATE(ts_main_event_field_val) = '${params.mdate}'
+    AND fk_state in ${params.statesOfInterest}
     ORDER BY id_control_unit_data`;
 
     return query;
