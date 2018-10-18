6116e708aca7be8005f99e8eb99773d0d110df6a
deeper-x <albertodeprezzo@gmail.com>
2018-10-18 19:22:54 +0200

    Archive calls: arrivals and departures

--- marmaduke-proj/routes/archive_data.js
+++ marmaduke-proj/routes/archive_data.js
@@ -5,5 +5,7 @@ let configuration = require("./data/config/proj_settings");
 
 router.get('/moored/:id/:startTimestamp/:stopTimestamp', callbacks.cbArchiveDataTable("archive_moored", configuration.STATES_OF_MOORING)),
 router.get('/roadstead/:id/:startTimestamp/:stopTimestamp', callbacks.cbArchiveDataTable("archive_roadstead", configuration.STATES_OF_ROADSTEAD)),
+router.get('/arrivals/:id/:startTimestamp/:stopTimestamp', callbacks.cbArchiveDataTable("archive_arrivals", configuration.STATES_OF_ARRIVALS)),
+router.get('/departures/:id/:startTimestamp/:stopTimestamp', callbacks.cbArchiveDataTable("archive_departures", configuration.STATES_OF_DEPARTURES)),
 
 module.exports = router;
